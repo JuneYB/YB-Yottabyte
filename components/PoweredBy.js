94c23bad34c426d5272a08501d058ad68e0ff3c7
JuneYB <165126423+JuneYB@users.noreply.github.com>
2026-03-05 15:00:21 +0800

    Change 'Eng.' to 'Ing.' in PoweredBy component

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,7 +8,7 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>© {new Date().getFullYear()}</span>
-      <span className='font-bold'>Eng. YB</span>
+      <span className='font-bold'>Ing. YB</span>
       <span className='ml-1 text-gray-400'>| Mechanical Engineering Archive</span>
     </div>
   )
